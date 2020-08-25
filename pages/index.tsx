17f560e2860cd6c238ba210004aee286cbee73ce
Raanan Weber <info@raananweber.com>
2020-08-25 14:01:17 +0200

    homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Head from 'next/head';
+import Layout from '../components/layout.component';
 
 export default function Home() {
   return (
-    <div>
+    <Layout isMarkdown={true} metadata={{
+      title: 'Babylon.js home',
+      description: 'Babylon.js documentation page',
+      keywords: 'babylonjs. documentation'
+    }} id={[]}>
       HOME
-    </div>
+      </Layout>
   )
 }
