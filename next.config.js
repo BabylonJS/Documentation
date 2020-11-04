9e22518ccb6bc458d2fc670b2974ff2f43a64ee8
Raanan Weber <info@raananweber.com>
2020-11-04 15:12:10 +0100

    adding sitemaps

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 // next.config.js
 const withSass = require("@zeit/next-sass");
 const withMDX = require("@next/mdx")({
-    // extension: /\.mdx?$/,
     options: {
-        remarkPlugins: [require('remark-slug'), require('remark-lint')],
-        rehypePlugins: [require('rehype-highlight')],
+        remarkPlugins: [require("remark-slug"), require("remark-lint")],
+        rehypePlugins: [require("rehype-highlight")],
     },
 });
 module.exports = withMDX(
@@ -19,5 +18,12 @@ module.exports = withMDX(
         //     importLoaders: 2,
         //     localIdentName: "[local]___[hash:base64:5]",
         // },
+        webpack: (config, { isServer }) => {
+            if (isServer) {
+                require("./lib/buildUtils/sitemap");
+            }
+
+            return config;
+        },
     }),
 );
