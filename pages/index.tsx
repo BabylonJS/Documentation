575fca3ace2a27f1d8d733afed8e68a733d4511d
Raanan Weber <info@raananweber.com>
2020-11-09 15:25:17 +0100

    search in search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
-import Layout from '../components/layout.component';
+import Head from "next/head";
+import Layout from "../components/layout.component";
 
 export default function Home() {
-  return (
-    <Layout breadcrumbs={[]} metadata={{
-      title: 'Babylon.js home',
-      description: 'Babylon.js documentation page',
-      keywords: 'babylonjs. documentation'
-    }} id={[]}>
-      HOME
-      </Layout>
-  )
+    return (
+        <Layout
+            breadcrumbs={[]}
+            metadata={{
+                title: "Babylon.js home",
+                description: "Babylon.js documentation page",
+                keywords: "babylonjs. documentation",
+            }}
+            id={[]}
+        >
+            <Head>
+                <script type="application/ld+json">
+                    {`{
+      "@context": "https://schema.org",
+      "@type": "WebSite",
+      "url": "https://docs.babylonjs.com/",
+      "potentialAction": {
+        "@type": "SearchAction",
+        "target": "https://docs.babylonjs.com/search?q={search_term_string}",
+        "query-input": "required name=search_term_string"
+      }
+    }`}
+                </script>
+            </Head>
+            <>HOME</>
+        </Layout>
+    );
 }
