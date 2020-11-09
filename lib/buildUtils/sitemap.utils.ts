c40ae010088065695c98b3caddedee97baefed9a
Raanan Weber <info@raananweber.com>
2020-11-09 17:29:00 +0100

    check sitemaps

--- lib/buildUtils/sitemap.utils.ts
+++ lib/buildUtils/sitemap.utils.ts
@@ -8,7 +8,7 @@ const tmpFile = resolve(tmpPath, `${process.pid}.xml`);
 const sitemapFile = resolve(basePath, `sitemap.xml`);
 
 const cache = [];
-let timeout;
+let timeout: NodeJS.Timeout;
 
 export const addToSitemap = (name: string, url: string, lastModified?: string) => {
     cache.push({
@@ -28,13 +28,13 @@ export const addToSitemap = (name: string, url: string, lastModified?: string) =
         });
         writeFileSync(tmpFile, endOfFile.join("\n"), { encoding: "utf-8" });
         writeAllToSitemap();
-    }, 600 * Math.random() + 200);
+    }, 500);
 };
 
 export const writeAllToSitemap = () => {
     const filenames = getAllFiles(tmpPath, [], ".xml");
     const results = filenames.map((fn) => readFileSync(fn).toString());
-    console.log('writing', filenames.join(','));
+    console.log('writing', filenames.length);
     const start = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url><loc>/</loc></url>
