067af52d2163531b13a750a3daa3803202ced6f1
Raanan Weber <info@raananweber.com>
2020-11-10 18:52:04 +0100

    powered by vercel (sponsorship)

--- lib/frontendUtils/frontendTools.ts
+++ lib/frontendUtils/frontendTools.ts
@@ -2,7 +2,7 @@ import { IExampleLink } from "../content.interfaces";
 
 export const getExampleLink = (example: Partial<IExampleLink>, full: boolean = true) => {
     const id = example.id ? (example.id[0] === "#" ? example.id : `#${example.id}`) : "";
-    return (example.type === "pg" ? ("https://playground.babylonjs.com/" + (full ? "full" : "")) : "https://nme.babylonjs.com/") + id;
+    return (example.type === "pg" ? ("https://playground.babylonjs.com/" + (full ? "full.html" : "")) : "https://nme.babylonjs.com/") + id;
 };
 
 export const getExampleImageUrl = (example: Partial<IExampleLink>) => {
