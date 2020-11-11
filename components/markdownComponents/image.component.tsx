083c7ce5781fd86f144f605f1cf3409491cc1f18
Raanan Weber <info@raananweber.com>
2020-11-11 12:53:41 +0100

    throttle on image resize

--- components/markdownComponents/image.component.tsx
+++ components/markdownComponents/image.component.tsx
@@ -1,7 +1,8 @@
-import { FunctionComponent, useState } from "react";
+import { createRef, FunctionComponent, useEffect, useRef, useState } from "react";
 import { IImageEmbed } from "../../lib/content.interfaces";
 import Image from "next/image";
 import { makeStyles, createStyles, Theme } from "@material-ui/core";
+import { throttle } from "../../lib/frontendUtils/frontendTools";
 
 const styles = makeStyles((theme: Theme) =>
     createStyles({
@@ -26,7 +27,35 @@ const styles = makeStyles((theme: Theme) =>
  */
 export const ImageMarkdownComponent: FunctionComponent<IImageEmbed> = (props) => {
     const [containerScale, setContainerScale] = useState<{ w: number; h: number }>({ h: 0, w: 0 });
+    const [intrinsic, setIntrinsic] = useState<{ w: number; h: number }>({ h: 0, w: 0 });
     const classes = styles();
+    const containerRef = useRef<HTMLImageElement>();
+    const onResize = () => {
+        console.log(intrinsic.h);
+        if (intrinsic.h === 0) {
+            return;
+        }
+        let { h, w } = intrinsic;
+        const markdownContainer = document.querySelector(".markdown-container") as HTMLDivElement;
+        const containerWidth =  markdownContainer.clientWidth - 32;
+        console.log(w, containerWidth);
+        if (w > containerWidth) {
+            h = (h * containerWidth) / w;
+            w = containerWidth;
+        }
+        setContainerScale({ h, w });
+    };
+    useEffect(() => {
+        if (containerScale.h === 0) {
+            return;
+        }
+        console.log("useEffect", props.src);
+        const resize = throttle(onResize, 100);
+        window.addEventListener("resize", resize, false);
+        return () => {
+            window.removeEventListener("resize", resize);
+        };
+    }, [intrinsic]);
     const getImage = () => {
         if (props.src.startsWith("http") || props.src.startsWith("//") || props.src.indexOf(".gif") !== -1) {
             return <img className={classes.image} {...props} />;
@@ -46,11 +75,14 @@ export const ImageMarkdownComponent: FunctionComponent<IImageEmbed> = (props) =>
                                 const imgTag = e.target as HTMLImageElement;
                                 let h = imgTag.naturalHeight;
                                 let w = imgTag.naturalWidth;
-                                if(imgTag.naturalWidth > imgTag.clientWidth) {
-                                    h = h * imgTag.clientWidth / w;
+                                if (imgTag.naturalWidth > imgTag.clientWidth) {
+                                    h = (h * imgTag.clientWidth) / w;
                                     w = imgTag.clientWidth;
                                 }
                                 setContainerScale({ h, w });
+                                if (intrinsic.h === 0) {
+                                    setIntrinsic({ h: imgTag.naturalHeight, w: imgTag.naturalWidth });
+                                }
                             } catch (e) {
                                 //no-op
                             }
@@ -66,7 +98,7 @@ export const ImageMarkdownComponent: FunctionComponent<IImageEmbed> = (props) =>
     };
 
     return (
-        <div style={{ height: containerScale.h !== 0 ? containerScale.h : "100%", maxWidth: containerScale.w !== 0 ? containerScale.w : "800px" }} className={classes.imageWrapper}>
+        <div ref={containerRef} style={{ height: containerScale.h !== 0 ? containerScale.h : "100%", width: containerScale.w !== 0 ? containerScale.w : "800px" }} className={classes.imageWrapper}>
             {getImage()}
             {props.caption && <span className={classes.caption}>{props.caption}</span>}
         </div>
