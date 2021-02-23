9c5cc9ebf5b6e3de29cf79cd79b2182ef4b96120
Raanan Weber <info@raananweber.com>
2021-02-23 11:36:22 +0100

    remove console output

--- lib/buildUtils/search.utils.ts
+++ lib/buildUtils/search.utils.ts
@@ -67,9 +67,6 @@ export const addSearchItem = async (searchItem: ISearchIndexItem) => {
 };
 
 export const addPlaygroundItem = async (item: IPlaygroundSearchItem) => {
-    if(item.isMain) {
-        console.log(item);
-    }
     if (!process.env.SEARCH_API_KEY) {
         return;
     }
