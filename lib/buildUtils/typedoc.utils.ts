4d06fbf343f382adadeb57bcda2409d480b1162e
Raanan Weber <Raanan.Weber@microsoft.com>
2022-06-14 13:40:44 +0200

    no .html needed

--- lib/buildUtils/typedoc.utils.ts
+++ lib/buildUtils/typedoc.utils.ts
@@ -120,7 +120,7 @@ export const getAPIPageData = async (id: string[]) => {
             filename = f.substring(f.indexOf(id[0]));
         });
         return {
-            redirect: `/typedoc/${filename}`,
+            redirect: `/typedoc/${filename.replace(".html", "")}`,
         };
     }
     const html = readFileSync(filename, "utf-8").toString();
