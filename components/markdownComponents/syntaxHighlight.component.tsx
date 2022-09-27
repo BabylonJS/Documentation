530fcd9304b618f622d782428d6c53c0aede1b5c
Raanan Weber <Raanan.Weber@microsoft.com>
2022-09-27 13:06:01 +0200

    Wrap code in... well... code.

--- components/markdownComponents/syntaxHighlight.component.tsx
+++ components/markdownComponents/syntaxHighlight.component.tsx
@@ -12,13 +12,15 @@ export const SyntaxHighlighting: FunctionComponent<{ className: string; children
         <Highlight {...defaultProps} theme={vsDark} code={props.children.trim()} language={language}>
             {({ className, style, tokens, getLineProps, getTokenProps }) => (
                 <pre className={className} style={{ ...style, padding: "20px" }}>
-                    {tokens.map((line, i) => (
-                        <div key={i} {...getLineProps({ line, key: i })}>
-                            {line.map((token, key) => (
-                                <span key={key} {...getTokenProps({ token, key })} />
-                            ))}
-                        </div>
-                    ))}
+                    <code>
+                        {tokens.map((line, i) => (
+                            <div key={i} {...getLineProps({ line, key: i })}>
+                                {line.map((token, key) => (
+                                    <span key={key} {...getTokenProps({ token, key })} />
+                                ))}
+                            </div>
+                        ))}
+                    </code>
                 </pre>
             )}
         </Highlight>
